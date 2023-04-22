45dd6aadf0dbbd900b99ef4f0ff5162091c7f142
Nep <38284741+Re3ses@users.noreply.github.com>
2023-04-22 17:01:07 +0800

    added working sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,14 +1,30 @@
-<div class="container sidebar">
-    <div class="sidebar-collapsed" id="sidebar-col">
-        <button onclick="expand()">
-            show
+<div class="sidebar-wrapper d-flex flex-column h-100" id="sidebar">
+    <div class="sidebar-extension d-flex flex-column align-items-center">
+        <button type="button" onClick="expand()" class="bg-transparent">
+            <i class="fa-solid fa-bars"></i>
         </button>
-    </div>
-    <div class="sidebar-expanded" id="sidebar-exp">
-        <button onclick="expand()">
-            show
-        </button>
+        <ol class="sidebar-btns d-flex flex-column align-items-center pt-4">
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">Home</span></a>
+            </li>
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">Routes</span></a>
+            </li>
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">Weather</span></a>
+            </li>
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">News</span></a>
+            </li>
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">Map</span></a>
+            </li>
+            <li>
+                <a class="fa-solid fa-house"><span class="sidebar-txt">About</span></a>
+            </li>
+        </ol>
     </div>
 </div>
+
 
 <script src="js/sidebar.js"></script>
