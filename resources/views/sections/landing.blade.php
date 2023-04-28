38372487b9674122e202298bfec9e64d6997a0e6
Nep <38284741+Re3ses@users.noreply.github.com>
2023-04-28 14:51:55 +0800

    moved sidebar and js code to respective blade files.

--- resources/views/sections/landing.blade.php
+++ resources/views/sections/landing.blade.php
@@ -27,4 +27,15 @@
 </div>
 
 
-<script src="js/landing.js"></script>
+<script>
+    function updateTime() {
+        fetch('https://worldtimeapi.org/api/ip')
+            .then(response => response.json())
+            .then(data => {
+                var timeString = data.datetime.slice(11, 16);
+                document.getElementById('clock').innerHTML = timeString;
+            });
+    }
+
+    setInterval(updateTime, 1000);
+</script>
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar-wrapper d-flex flex-column h-100" id="sidebar">
     <div class="sidebar-extension d-flex flex-column align-items-center justify-content-between ">
-        <button type="button" onClick="expand()" class="bg-transparent">    
+        <button type="button" onClick="expand()" class="bg-transparent">
             <i class="fa-solid fa-bars"></i>
         </button>
         <ol class="sidebar-btns d-flex flex-column align-items-center pt-4">
@@ -29,8 +29,52 @@
             </button>
         </a>
     </div>
-   
+
 </div>
 
 
-<script src="js/sidebar.js"></script>
+<script>
+    let sidebar = document.getElementById("sidebar");
+    let hiddenElements = document.querySelectorAll(".sidebar-txt");
+    let sidebarButtons = document.querySelectorAll(".sidebar-button");
+    let width = 4;
+    let hidden = true;
+
+    function expand() {
+        animate(hidden);
+
+        hiddenElements.forEach(element => {
+            if (hidden) {
+                element.style.display = 'inline';
+            } else {
+                element.style.display = 'none';
+            }
+        });
+
+        sidebarButtons.forEach(button => {
+            if (hidden) {
+                button.style.width = width + "vw";
+            } else {
+                button.style.width = 13 + "vw";
+            }
+        });
+
+        hidden = !hidden;
+    }
+
+    function animate(toExpand) {
+        if (toExpand === true) {
+            width++;
+            sidebar.style.width = width + "vw";
+            if (width < 13) {
+                requestAnimationFrame(() => animate(toExpand));
+            }
+        } else {
+            width--;
+            if (width >= 4) {
+                sidebar.style.width = width + "vw";
+                requestAnimationFrame(() => animate(toExpand));
+            }
+        }
+    }
+</script>
