3763c885efc8fd0b513c2c9fe487d00c4e086ecd
Nep <38284741+Re3ses@users.noreply.github.com>
2023-04-28 14:50:44 +0800

    added routes page selector

--- resources/views/sections/routes.blade.php
+++ resources/views/sections/routes.blade.php
@@ -1,6 +1,16 @@
 <div class="container-fluid p-5 h-100">
-    <div class="row table-bg rounded h-100">
-        <table class="table text-white fw-medium">
+    <div class="row table-bg rounded h-100 flex-column">
+        <div class="col-2 py-3">
+            <select class="routeSelector form-select form-select-sm " name="routeSelector" id="routeSelector" aria-label="Default select example">
+                <option selected>Select Location</option>
+                <option value="1">Daet</option>
+                <option value="2">Iriga</option>
+                <option value="3">Lagonoy</option>
+                <option value="4">Legazpi</option>
+                <option value="5">Polangui</option>
+            </select>
+        </div>
+        <table class="col-12 table text-white fw-medium">
             <thead>
                 <th scope="col">Bus No.</th>
                 <th scope="col">Departure Time</th>
