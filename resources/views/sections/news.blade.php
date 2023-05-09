9c535ea745304440e3dc48649b0f4b9ac3a452f0
Nep <38284741+Re3ses@users.noreply.github.com>
2023-05-10 03:05:54 +0800

    made news section mock up

--- resources/views/sections/news.blade.php
+++ resources/views/sections/news.blade.php
@@ -1,10 +1,51 @@
 <div class="container-fluid flex-column p-5 h-100 w-100">
-    <div class="row px-3">
+    <div class="row px-3 text-white fw-bold">
         <h1 class="fs-1 col-1 me-5">News</h1>
     </div>
     <div class="row px-5">
-        <div class="col">
-            Announcements go here
+        <div class="row">
+            <div class="col-4">
+                <div class="row newsBit">
+                    <h3>City to Launch New Bus Rapid Transit System</h3>
+                    <p>The city announces the launch of a new bus rapid transit system that will offer faster and more efficient service to commuters.</p>
+                </div>
+                <div class="row newsBit">
+                    <h3>Road Safety Campaign Aims to Reduce Accidents</h3>
+                    <p>A new road safety campaign has been launched, with the aim of reducing the number of accidents and fatalities on the city's roads.</p>
+
+                </div>
+            </div>
+            <div class="col-2 newsBit">
+                <h3>Love</h3>
+                <p>
+                    I've finally fallen in love with the person I want and I am so happy right now. She makes me feel
+                    loved and appreciated and so much more.
+                </p>
+                <h3>Patience</h3>
+                <p>
+                    It's hard to keep calm.
+                </p>
+            </div>
+            <div class="col-4 newsBit">
+                <h3>Department of Public Works and Highways Completes Rehabilitation of Major Highway</h3>
+                <p>The Department of Public Works and Highways announces the completion of the rehabilitation of a major highway in the Philippines. The project includes the repair of road surfaces, bridges, and drainage systems, which will improve safety and reduce travel time for motorists.</p>
+            </div>
+            <div class="col-1 newsBit">
+                <h3>Shoutout!</h3>
+                <p>Hello to everyone who's reading this. I really tried making stuff that is related to the site but I'm bored. My brain isn't really working well but it's still functional. Thank you for reading this that's all.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 newsBit">
+                <h3>This isn't working still tbh</h3>
+                <p>It could be due to lack of time, procrastination, or some other reason that to be honest, isn't really valid. Nonetheless here is a mock of what this is supposed to be. I'm writing this to have a really long message here. But apparantly, it's not long enough so that's why I'm writing this as well. <br>
+                Did you know that I am writing this at exactly 2:58 am? Yeah I need to make this so that I can show this very words I'm saying.</p>
+            </div>
+            <div class="col-5 newsBit">
+                <h3>Keep up to date here for the latest uh head ramblings...</h3>
+                <h4>Here have some lorem</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum mollitia alias ea dolore laborum, culpa sapiente eveniet amet ipsam cumque dicta earum praesentium ab soluta ipsa exercitationem. Deleniti, distinctio dolores.</p>
+            </div>
         </div>
     </div>
 </div>
