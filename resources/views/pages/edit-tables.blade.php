f27ff233789f61d636616fa98097646059190120
Nep <38284741+Re3ses@users.noreply.github.com>
2023-05-10 02:00:05 +0800

    added edit and delete buttons for trips in table

--- resources/views/pages/edit-tables.blade.php
+++ resources/views/pages/edit-tables.blade.php
@@ -30,7 +30,7 @@
                             @endforeach
 
                         </select>
-                        <label for="departure">Departure TimeS</label>
+                        <label for="departure">Departure Time</label>
                         <input type="time" name="departure" id="departure">
                         <input type="submit" value="ADD">
                     </form>
@@ -39,6 +39,7 @@
 
             <hr>
 
+            {{-- Routes table --}}
             <div class="container-fluid px-5">
                 {{-- Label and select tag --}}
                 <div class="row px-3">
@@ -54,10 +55,12 @@
                         </select>
                     </div>
                 </div>
+
                 <div class="row table-bg rounded flex-row py-3">
                     {{-- Trips display table --}}
                     <div class="table-responsive table-body">
-                        <table class="col-12 table table-dark table-hover table-striped table-sm text-white fw-medium" id="routeTable">
+                        <table class="col-12 table table-dark table-hover table-striped table-sm text-white fw-medium"
+                            id="routeTable">
                             <thead>
                                 <th scope="col">Bus No.</th>
                                 <th scope="col">Departure Time</th>
@@ -66,6 +69,8 @@
                                 <th scope="col">Fare</th>
                                 <th scope="col">Driver</th>
                                 <th scope="col">Bus Liner</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
                             </thead>
                             <tbody>
                                 {{-- Display table data via blade templating --}}
@@ -78,17 +83,71 @@
                                         <td>{{ $row->bus_routes->route_fare ?? 'N/A' }}/ km</td>
                                         <td>{{ $row->bus_data->bus_driver ?? 'N/A' }}</td>
                                         <td>{{ $row->bus_data->bus_company ?? 'N/A' }}</td>
+                                        <td>
+                                            <div class="h-100 w-100">
+                                                <button type="button" class="btn btn-primary btn-sm w-100"
+                                                    data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <form action="/edit-tables/delete/{{ $row->bus_data->bus_numer ?? null }}"
+                                                class="d-flex align-items-center">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
+
+                <!-- Modal -->
+                <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="editModalLabel">Edit Trip</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="edit-bus-number" class="col-form-label py-1">Bus Number:</label>
+                                        <input type="text" value="insert code for bus number ehre" class="form-control" id="edit-bus-number" disabled>
+                                        <label for="edit-departure" class="py-1">Bus Departure: </label>
+                                        <input type="time" class="py-1" name="departure" id="edit-departure">
+                                    </div>
+                                    <label for="edit-destination">Destination</label>
+                                    <div class="w-100 col-2 py-3 d-flex align-items-center justify-content-center">
+                                        <select class="form-select form-select-sm" id="edit-destination"
+                                            aria-label="Default select example" required>
+                                            <option value="" selected>Select Location</option>
+                                            <option value="Daet">Daet</option>
+                                            <option value="Iriga">Iriga</option>
+                                            <option value="Lagonoy">Lagonoy</option>
+                                            <option value="Legaspi">Legaspi</option>
+                                            <option value="Polangui">Polangui</option>
+                                        </select>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" class="btn btn-primary" id="save-changes-button">Save Changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
             <script>
                 // Listen for changes to the select element
-            $('#routeSelector').on('change', function() {
+                $('#routeSelector').on('change', function() {
                     // Get the selected option value
                     var selectedOption = $(this).val();
 
@@ -99,7 +158,9 @@
                             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                         },
                         url: '/routes/' + selectedOption, // Replace with the URL to your PHP script
-                        data: { route: selectedOption },
+                        data: {
+                            route: selectedOption
+                        },
                         success: function(response) {
                             console.log('success:', response);
                             // Update the table with the new data
